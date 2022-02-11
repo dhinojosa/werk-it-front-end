100c6859bfb29e8218516f4d8b8a16b8ed707fa8
MarcKiT <marckitmail@gmail.com>
2022-02-11 01:24:29 -0500

    Sticky footer added to the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <h1 class="text-6xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <body className="flex flex-col h-screen">
+      <header>
+        <Navbar />
+      </header>
+      <main className="flex-grow">
+        <h1 className="text-6xl font-bold underline ">
+          Hello world!
+        </h1>
+      </main>
+      <footer className="flex flex-wrap items-center justify-between px-2 py-3 bg-amber-500">
+        <Footer />
+      </footer>
+    </body>
   );
 }
 
---
+++ src/components/footer.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Footer() {
+       return( <h1>© To That Girl Who Great Ideas</h1>)
+}
