20f3de35f03bed88f0a17d94ae674cb87e46940f
MarcKiT <marckitmail@gmail.com>
2022-02-11 07:37:45 -0500

    src/App.js basic Router implementation 5 pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import Navbar from "./components/navbar"
 import Footer from "./components/footer"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Exercises from "./pages/Exercises"
+import Nutrition from "./pages/Nutrition"
+import Profile from "./pages/Profile"
+import ErrorPage from "./pages/ErrorPage"
 
 function App() {
   return (
     <body className="flex flex-col h-screen">
-      <header>
-        <Navbar />
-      </header>
-      <main className="flex-grow">
-        <h1 className="text-6xl font-bold underline ">
-          Hello world!
-        </h1>
-      </main>
+      <Router>
+        <header>
+          <Navbar />
+        </header>
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/exercises" element={<Exercises />} />
+            <Route path="/nutrition" element={<Nutrition />} />
+            <Route path="/profile/:username" element={<Profile />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </main>
+      </Router>
       <footer className="flex flex-wrap items-center justify-between px-2 py-3 bg-amber-500">
         <Footer />
       </footer>
