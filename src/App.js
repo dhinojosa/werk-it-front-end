79ebbeaa13f14a8d9fdc3de4e06955052d736b52
Marc-Andre Labelle <marckitdev@gmail.com>
2022-02-12 22:12:19 -0500

    Package + global + import added for Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
