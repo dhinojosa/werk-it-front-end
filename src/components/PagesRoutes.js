d537662f4e3bef36001e32eabd59ccf8cde74ca8
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-02-14 08:22:07 -0700

    Added Route for exercises

--- src/components/PagesRoutes.js
+++ src/components/PagesRoutes.js
@@ -4,12 +4,14 @@ import ErrorPage from "../pages/ErrorPage";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
 import Profile from "../pages/Profile";
+import Exercises from "../pages/Exercises";
 
 function PagesRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/exercises" element={<Exercises />} />
       <Route path="/profile/:username" element={<Profile />} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
