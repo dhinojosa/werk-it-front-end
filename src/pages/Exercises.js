547764efc53388049e1662e73d999c3900a7b6cf
Marc-Andre Labelle <marckitdev@gmail.com>
2022-02-14 01:38:30 -0500

    Cards fitting in the page

--- src/pages/Exercises.js
+++ src/pages/Exercises.js
@@ -1,7 +1,65 @@
 import React from "react";
+import { Container, Row, Card, Button } from "react-bootstrap";
+import { FaRunning } from "react-icons/fa";
 
 function Exercises() {
-  return <div>THIS IS THE EXERCISES PAGE</div>;
+  return (
+    <Container className="m-12 justify-content-center">
+      <h1 className="mt-4">
+        <FaRunning />
+        Exercises
+      </h1>
+      <Row className="my-5">
+        <Card className="m-3" bg="light" style={{ width: "18rem" }}>
+          <Card.Img className="mt-2" variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className="m-3" bg="light" style={{ width: "18rem" }}>
+          <Card.Img className="mt-2" variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className="m-3" bg="light" style={{ width: "18rem" }}>
+          <Card.Img className="mt-2" variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className="m-3" bg="light" style={{ width: "18rem" }}>
+          <Card.Img className="mt-2" variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Row>
+    </Container>
+  );
 }
 
 export default Exercises;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,64 @@
 import React from "react";
+import { Container, Row, Card, Button } from "react-bootstrap";
+import { BsCheck2Circle } from "react-icons/bs";
 
 function Home() {
-  return <div> THIS IS THE HOME PAGE</div>;
+  return (
+    <Container className="m-12 justify-content-center">
+      <h1 className="mt-4">
+        <BsCheck2Circle /> Objectives
+      </h1>
+      <Row className="my-5">
+        <Card className="m-3" bg="light" style={{ width: "18rem" }}>
+          <Card.Img className="mt-2" variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className="m-3" bg="light" style={{ width: "18rem" }}>
+          <Card.Img className="mt-2" variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className="m-3" bg="light" style={{ width: "18rem" }}>
+          <Card.Img className="mt-2" variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className="m-3" bg="light" style={{ width: "18rem" }}>
+          <Card.Img className="mt-2" variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Row>
+    </Container>
+  );
 }
 
 export default Home;
