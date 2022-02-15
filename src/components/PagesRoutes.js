5f6c02fc57d16ce73033b33bacc54580e27e1ed9
Marc-Andre Labelle <marckitdev@gmail.com>
2022-02-15 10:11:50 -0500

    Add the the Nutrition page to the webpage routing

--- src/components/PagesRoutes.js
+++ src/components/PagesRoutes.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import ErrorPage from "../pages/ErrorPage";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
+import Nutrition from "../pages/Nutrition";
 import Profile from "../pages/Profile";
 import Exercises from "../pages/Exercises";
 import Login from "../pages/Login";
@@ -16,6 +17,7 @@ function PagesRoutes() {
       <Route path="/login" element={<Login />} />
       <Route path="/recover-password" element={<RecoverLogin />} />
       <Route path="/exercises" element={<Exercises />} />
+      <Route path="/nutrition" element={<Nutrition />} />
       <Route path="/profile/:username" element={<Profile />} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
